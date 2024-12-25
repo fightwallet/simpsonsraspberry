1cc6d2c1fcb0f8892c9eb8b72a874c570b5314b0
blackwater <superberner@gmail.com>
2024-12-24 19:23:06 -0800

    Create encoder.py

---
+++ encoder.py
@@ -0,0 +1,39 @@
+import os
+
+newFiles = []
+directory = os.path.dirname(os.path.realpath(__file__))
+destinationDirectory = os.path.join(directory, 'encoded')
+
+if not os.path.exists(destinationDirectory):
+	os.mkdir(destinationDirectory)
+
+
+def isVideo(videofile):
+	if videofile.lower().endswith('.mp4'):
+		return True
+	if videofile.lower().endswith('.mkv'):
+		return True
+	if videofile.lower().endswith('.mov'):
+		return True
+	if videofile.lower().endswith('.avi'):
+		return True
+	return False
+
+newFiles = [os.path.join(dp, f) for dp, dn, filenames in os.walk(directory) for f in filenames if isVideo(f)]
+
+for filepath in newFiles:
+	video = os.path.basename(filepath)
+	videoName = os.path.splitext(video)[0]
+	newFile = '%s.mp4' % videoName
+	i = filepath
+	o = os.path.join(destinationDirectory, newFile)
+	if os.path.isfile(o):
+		continue
+	
+    #OG 
+    #encodeCommand = 'ffmpeg -i "%s" -vf scale=-2:480 -c:v libx264 -profile:v baseline -level 3.0 -preset fast -crf 23 -pix_fmt yuv420p "%s"' % (i, o)
+	#first optimization 
+    #encodeCommand = 'ffmpeg -i "%s" -vf scale=-2:360 -r 24 -c:v libx264 -profile:v baseline -level 3.0 -preset slow -crf 28 -pix_fmt yuv420p -c:a aac -b:a 64k -ar 44100 "%s"' % (i, o)
+	encodeCommand = 'ffmpeg -i "%s" -vf scale=-2:480 -r 24 -c:v libx264 -profile:v baseline -level 3.0 -preset slow -crf 24 -pix_fmt yuv420p -c:a aac -b:a 96k -ar 44100 "%s"' % (i, o)
+	print('Encoding %s' % newFile)
+	encode = os.popen(encodeCommand).read()
